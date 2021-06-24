c74cdb2b561daf8bdaf4e46250723c8e697a2dbf
Adil Salim <1awesomeadil@gmail.com>
2021-06-24 10:25:45 +0200

    Work done for the day

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function(lst, tRm) {
-    return lst.splice(tRm, 1);
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+  const newArray = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 module.exports = removeFromArray;
