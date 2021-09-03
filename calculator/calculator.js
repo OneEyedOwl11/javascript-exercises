acd7a7853784bda4fd7833aeb09d4d4803515633
Adil Salim <1awesomeadil@gmail.com>
2021-09-03 10:30:18 +0200

    Passed Calculator Tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let sums = 0;
+  for (let i = 0; i<x.length; i++) {
+    sums += x[i];
+  }
+  return sums;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let sums = 1;
+  for (let i = 0; i<x.length; i++) {
+    sums *= x[i];
+  }
+  return sums;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
+const factorial = function(x) {
+let result = 1;
+  if (x==1 || x == 0) {
+    return 1;
+  }
+
+  else {
+    for (let i = 1; i<=x; i++) {
+      result *= i;
+    }
+  }
+  return result;
 	
 };
 
