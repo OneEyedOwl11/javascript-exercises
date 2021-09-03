d662d9f869779c59a538f3b8c5c594f088b7f4c5
Adil Salim <1awesomeadil@gmail.com>
2021-09-03 11:02:24 +0200

    Palindromes Done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    let re = /[\W_]/g;
 
+    let origin = x.toLowerCase().replace(re, '');
+    let result = origin.split('').reverse().join('');
+
+    if (origin == result) {
+        return true;
+    }
+
+    else {
+        return false
+    }
 };
 
 module.exports = palindromes;
