c2d004b7c677d8fb79648c7619de584665afe0d2
Adil Salim <1awesomeadil@gmail.com>
2021-09-07 11:52:58 +0200

    Fibionacci done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (parseInt(x) == 1 || parseInt(x) == 0) {
+            return 1
+        }
+    else if (parseInt(x) > 1) {
+        let num1 = 0;
+        let num2 = 1;
+        let sum;
+    for (let i = 1; i < parseInt(x); i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        }
+    return sum;
+    }
+    else {
+        return "OOPS"
+    }
 };
 
 module.exports = fibonacci;
